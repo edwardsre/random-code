c7d091b30283c55b53d60f46c794998b4513a6fc
Rob Edwards <edwardsre@familysearch.org>
2015-01-16 11:26:38 -0700

    Implement custom enum

---
+++ src/main/java/apprentice/java/EuropeanArtists.java
@@ -0,0 +1,17 @@
+package apprentice.java;
+
+public enum EuropeanArtists {
+
+  PICASO,
+  VAN_GOGH,
+  MATISSE,
+  MONET,
+  MICHELANGELO,
+  DURER,
+  REMBRANDT,
+  CEZANNE,
+  DEGAS,
+  RAPHAEL,
+  VERMEER;
+
+}
